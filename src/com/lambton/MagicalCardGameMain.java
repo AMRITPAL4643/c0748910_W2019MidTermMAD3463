7291dfa96e3502ee2dc7af14d2ae9c040f2c0fe8
Amritpal4643 <amritpalsingh3027@gmail.com>
2019-01-31 20:47:59 -0500

    CHANGES IN DATA ENTR

--- src/com/lambton/MagicalCardGameMain.java
+++ src/com/lambton/MagicalCardGameMain.java
@@ -4,7 +4,7 @@ public class MagicalCardGameMain {
     public static void main(String args[]) {
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
         System.out.println(" Card Game ..");
-        String[][] cards = {{"1", "A", "5"}, {"5", "3", "K"}, {"6", "3", "K"}};
+        String[][] cards = {{"A", "7", "6"}, {"5", "3", "K"}, {"1", "2", "3"}};
         System.out.println(" select one card from below");
         for (int i = 0; i < cards.length; i++) {
             for (int j = 0; j < cards.length; j++) {
