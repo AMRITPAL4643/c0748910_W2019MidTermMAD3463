b3dd716555a82d63379af0ccb90d99308b47036b
Amritpal4643 <amritpalsingh3027@gmail.com>
2019-01-31 20:07:13 -0500

    OUTPUT OF MATRIX

--- src/com/lambton/MagicalCardGameMain.java
+++ src/com/lambton/MagicalCardGameMain.java
@@ -1,13 +1,21 @@
 package com.lambton;
-
+import java.util.Scanner;
 public class MagicalCardGameMain {
-
-    public static void main (String args[])
-    {
+    public static void main(String args[]) {
         MagicalCardGameModel magicalCardGameModel = new MagicalCardGameModel();
-        System.out.println("")
-        String [][]cards = {{"1","A","5"},{"2","7","3"},{"3","6","K"}};
+        System.out.println(" Card Game ..");
+        String[][] cards = {{"1", "A", "5"}, {"2", "7", "3"}, {"3", "6", "K"}};
+        System.out.println(" select one card from below");
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards.length; j++) {
+                System.out.print(cards[i][j]);
+                System.out.print(" , ");
+            }
+            System.out.println(" ");
+        }
         magicalCardGameModel.setCardList(cards);
-
+        Scanner scanner = new Scanner(System.in);
+        String FirstShuffleColPos = scanner.nextLine();
+        magicalCardGameModel.setFirstShuffleColPos(FirstShuffleColPos);
     }
 }
