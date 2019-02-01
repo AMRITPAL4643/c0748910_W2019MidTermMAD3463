a9c202aa169edc7891d2ec0b8294a6ffb7e5368b
Amritpal4643 <amritpalsingh3027@gmail.com>
2019-01-31 20:33:24 -0500

    changes

--- src/com/lambton/MagicalCardGameCalculator.java
+++ src/com/lambton/MagicalCardGameCalculator.java
@@ -8,12 +8,8 @@ public class MagicalCardGameCalculator {
         String[][] cards1 = magicalCardGameModel.getCardList();
         if (magicalCardGameModel.firstShuffleColPos.contains("A")) {
             rowNumber = 0;
-            for(int y=0; y<cards1.length;y++){
-                for(int u=0,u<cards1.length;u++){
-                    tempArray[y][u]
-                }
+            for(int y=0; y<cards1.length;y++) {
             }
-
         } else if (magicalCardGameModel.firstShuffleColPos.contains("B")) {
             rowNumber = 1;
         } else if (magicalCardGameModel.firstShuffleColPos.contains("C")) {
