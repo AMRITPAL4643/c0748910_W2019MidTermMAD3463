cdcb693c003cb72464b5d79a73ceea372048a381
Amritpal4643 <amritpalsingh3027@gmail.com>
2019-01-31 21:23:55 -0500

    data addition

--- src/com/lambton/MagicalCardGameCalculator.java
+++ src/com/lambton/MagicalCardGameCalculator.java
@@ -37,6 +37,18 @@ public class MagicalCardGameCalculator {
             tempArray[2][2]=cards1[2][2];
         } else if (magicalCardGameModel.firstShuffleColPos.contains("C")) {
             rowNumber = 2;
+            String tempArray[][]={};
+            tempArray[0][0]=cards1[0][1];
+            tempArray[0][1]=cards1[1][1];
+            tempArray[0][2]=cards1[2][1];
+
+            tempArray[1][0]=cards1[0][2];
+            tempArray[1][1]=cards1[1][2];
+            tempArray[1][2]=cards1[2][2];
+
+            tempArray[2][0]=cards1[0][0];
+            tempArray[2][1]=cards1[1][0];
+            tempArray[2][2]=cards1[2][0];
         }
         return cards1;
     }
