ada06846c81db442b6ab5e83ec9bc3dc7a27e538
Amritpal4643 <amritpalsingh3027@gmail.com>
2019-01-31 21:15:50 -0500

    changes in calculator calss

--- src/com/lambton/MagicalCardGameCalculator.java
+++ src/com/lambton/MagicalCardGameCalculator.java
@@ -8,8 +8,19 @@ public class MagicalCardGameCalculator {
         String[][] cards1 = magicalCardGameModel.getCardList();
         if (magicalCardGameModel.firstShuffleColPos.contains("A")) {
             rowNumber = 0;
-            for(int y=0; y<cards1.length;y++) {
-            }
+            String tempArray[][]={};
+            tempArray[0][0]=cards1[0][1];
+            tempArray[0][1]=cards1[1][1];
+            tempArray[0][2]=cards1[2][1];
+
+            tempArray[1][0]=cards1[0][0];
+            tempArray[1][1]=cards1[1][0];
+            tempArray[1][2]=cards1[2][0];
+
+            tempArray[2][0]=cards1[0][2];
+            tempArray[2][1]=cards1[1][2];
+            tempArray[2][2]=cards1[2][2];
+
         } else if (magicalCardGameModel.firstShuffleColPos.contains("B")) {
             rowNumber = 1;
         } else if (magicalCardGameModel.firstShuffleColPos.contains("C")) {
